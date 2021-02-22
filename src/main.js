6962854111d6876dec4b756cf30d4b30a201785c
Madhan R <madhan4r@gmail.com>
2021-02-22 18:25:36 +0530

    login auth

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import CoreuiVue from '@coreui/vue';
+import CoreuiVue from "@coreui/vue";
 
 Vue.use(CoreuiVue);
 Vue.config.productionTip = false;
 
 /* Import the firebase SDK and extend with firestore */
-import firebase from 'firebase';
-require('firebase/firestore');
+import firebase from "firebase/app";
+require("firebase/firestore");
 
 /* Paste your firebase configuration below */
 var firebaseConfig = {
@@ -19,7 +19,7 @@ var firebaseConfig = {
   projectId: "ngm4contacts",
   storageBucket: "ngm4contacts.appspot.com",
   messagingSenderId: "786370858500",
-  appId: "1:786370858500:web:6f11b10c9530db21d26f51",
+  appId: "1:786370858500:web:6f11b10c9530db21d26f51"
 };
 
 /* Initialize firebase with your configuration */
